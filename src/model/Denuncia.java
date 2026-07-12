f3d6cf78091c69a942aa33fd38e09891fdd56ac4
Iago Cleber <iagocleber@gmail.com>
2026-07-12 14:45:31 -0300

    feat: adicionar classe base Denuncia com validacao de segurança

---
+++ src/model/Denuncia.java
@@ -0,0 +1,28 @@
+package model;
+
+public class Denuncia {
+    private String protocolo;
+    private String descricaoFatos;
+    private String status; // "Recebida","EM_Analise","Arquivada","Deferida"
+
+    public Denuncia(String protocolo, String descricaoFatos, String status) {
+        if(descricaoFatos==null || descricaoFatos.trim().isEmpty()){
+            throw new IllegalArgumentException("descricaoFatos nao pode estar em branco");
+        }
+        this.protocolo = protocolo;
+        this.descricaoFatos = descricaoFatos;
+        this.status = "RECEBIDA";
+
+    }
+    public String gerarRelatorioAuditoria(){
+        return "=== Relatorio de triagem ===\n"+
+                "Protocolo: " + protocolo + "\n"+
+                "Status: " + status + "\n"+
+                "Relato: " + descricaoFatos + "\n";
+    }
+
+    public String getProtocolo() {return protocolo;}
+    public String getDescricaoFatos() {return descricaoFatos;}
+    public String getStatus() {return status;}
+    public void setStatus(String status) {this.status = status;}
+}
